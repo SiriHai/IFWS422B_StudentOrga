d428a6a75610f1057614781274e2f8048d91ad5f
SiriHai <iris.hanheide@fhdw.de>
2023-06-26 12:22:36 +0200

    Standardmethoden

--- src/controller/LecturerCtr.java
+++ src/controller/LecturerCtr.java
@@ -1,30 +1,16 @@
 package controller;
 
 import model.Lecturer;
-import model.Person;
 
 public class LecturerCtr {
     public static void main(String[] args) {
         Lecturer lect1 = new Lecturer("Liese", "Prysselius");
-        System.out.println(lect1.info());
+        System.out.println(lect1);
 
         Lecturer lect2 = new Lecturer("Liese", "Prysselius");
-        System.out.println(lect2.info());
+        System.out.println(lect2);
 
-        // Einlesen von der Konsole
-
-        // // Neu: Aufruf von newInputDialogue aus Person
-        // // 1. Instanmethode
-        // Lecturer lect3 = new Lecturer();
-        // lect3.newInputDialogue();
-        // System.out.println(lect3.info());
-
-        // 2. statische Methode
-        Lecturer lect4 = new Lecturer();
-        // Lecturer.newInputDialogue(lect4);
-        // wurde Klasse Person importiert ist auch folgendes möglich
-        Person.newInputDialogue(lect4);
-        System.out.println(lect4.info());      
+        System.out.println(lect1.equals(lect2));   
         
     }
 
