87209a7fd727a6c80b5ad07e63c16eec5721fbab
SiriHai <iris.hanheide@fhdw.de>
2023-08-14 17:33:04 +0200

    Überladen

--- src/model/Students.java
+++ src/model/Students.java
@@ -1,27 +1,41 @@
 package model;
 
 public class Students {
-
     private Student[] studentArray;
+    int anzahl;
 
     public Students(int anzahl) {
         studentArray = new Student[anzahl];
+        anzahl = 0;
     }
 
     public void add(Student student) {
-        for (int i = 0; i < studentArray.length; i++) {
-            if (studentArray[i] == null) {
-                studentArray[i] = student;
-                return;
-            }
-        }
-        System.out.println("Kein Platz mehr.");
+        if (anzahl < studentArray.length)
+            studentArray[anzahl++] = student;
+        else
+            System.out.println("Kein Platz mehr.");
     }
 
     public void remove(Student student) {
-        for (int i = 0; i < studentArray.length; i++) {
+        for (int i = 0; i < anzahl; i++) {
             if (studentArray[i] != null && studentArray[i].equals(student)) {
-                studentArray[i] = null;
+                for (int j = i; j < anzahl; j++) {
+                    studentArray[j] = studentArray[j + 1];
+                }
+                // anzahl-- reicht auch
+                studentArray[anzahl--] = null;
+                return;
+            }
+        }
+        System.out.println("Student nicht gefunden.");
+    }
+
+    public void remove(String firstName, String lastName) {
+        for (int i = 0; i < anzahl; i++) {
+            if (studentArray[i] != null && 
+                studentArray[i].getFirstName().equals(firstName) &&
+                studentArray[i].getLastName().equals(lastName)) {
+                    remove(studentArray[i]);
                 return;
             }
         }
@@ -31,10 +45,8 @@ public class Students {
     // Ausgabe aller gespeicherten Objekte
     public void print() {
         System.out.println("Studenten in students");
-        for (Student student : studentArray) {
-            if (student != null)
-                System.out.println(student);
+        for (int i = 0; i<anzahl; i++) {
+            System.out.println(studentArray[i]);
         }
     }
-
 }
